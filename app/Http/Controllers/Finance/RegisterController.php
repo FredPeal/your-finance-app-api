050ea82a541e66325c0c3e0ceb851ecd03ac4073
= <=>
2019-12-11 08:40:05 -0400

    Working with email register

---
+++ app/Http/Controllers/Finance/RegisterController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Finance;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use DB;
+use Exception;
+
+class RegisterController extends Controller
+{
+    //
+    public function register(RegisterRequest $request)
+    {
+        DB::beginTransaction();
+        try {
+            DB::commit();
+
+        } catch (Exception $e) {
+            DB::rollback();
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
+}
