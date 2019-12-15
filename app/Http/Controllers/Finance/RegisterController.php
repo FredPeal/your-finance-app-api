b36a63c543d78af5ad17c819f00f05ef615a1691
FredPeal <frederickpeal@gmailc.om>
2019-12-15 16:19:37 -0400

    Finish sign up and add docs

--- app/Http/Controllers/Finance/RegisterController.php
+++ app/Http/Controllers/Finance/RegisterController.php
@@ -6,18 +6,26 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
 use DB;
 use Exception;
+use App\User;
+use App\Mail\RegisterEmail;
+use Illuminate\Support\Facades\Mail;
 
 class RegisterController extends Controller
 {
     //
     public function register(RegisterRequest $request)
     {
+        
         $request->validated();
 
         DB::beginTransaction();
         try {
-            DB::commit();
-
+            $code = uniqid();
+            $data = $request->toArray();
+            $data['code_activation'] = $code;
+            $user = User::create($data);
+            Mail::to($user->email)->send(new RegisterEmail($user));
+            // DB::commit();
         } catch (Exception $e) {
             DB::rollback();
             return response()->json(['message' => $e->getMessage()], 500);
--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', 'Finance\RegisterController@register');
+Route::post('/sign-up', 'Finance\RegisterController@register');
